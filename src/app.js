f2f4554c66ecbff8d73d0912035003ca95a5285b
Tamas Galffy <ezittgtx@gmail.com>
2019-10-05 19:12:38 +0200

    Add ping endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-function main() {
-    console.log("Hello world!")
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+function setupRouters () {
+  const ping = require('./routes/ping')
+
+  console.log('Registering ping')
+  app.use('/ping', ping)
+}
+
+function main () {
+  setupRouters()
+
+  console.log(`Listening on port ${PORT}`)
+  app.listen(PORT)
 }
 
 main()
