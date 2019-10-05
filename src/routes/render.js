91cc2d73f2e5d2f348c3db8d8e95b4f884765bd5
Tamas Galffy <ezittgtx@gmail.com>
2019-10-05 22:23:35 +0200

    Render images in ASCII

--- src/routes/render.js
+++ src/routes/render.js
@@ -1,13 +1,18 @@
-const express = require('express')
 const fs = require('fs')
 const fsp = fs.promises
+const path = require('path')
+const express = require('express')
+const cheerio = require('cheerio')
 
 const transformMarkdown = require('../transforms/markdown')
+const transformImage = require('../transforms/image')
 
 const router = express.Router()
 
 router.get('/*', async (req, res) => {
   const file = req.path
+  const basedir = path.dirname(file)
+  console.log('Rendering', file)
 
   if (!fs.existsSync(file)) {
     res.status(404)
@@ -15,10 +20,27 @@ router.get('/*', async (req, res) => {
   }
 
   const data = await fsp.readFile(file, 'utf-8')
-  const result = transformMarkdown(data)
+  const html = transformMarkdown(data)
+
+  const $ = cheerio.load(html)
+
+  const imagePromises = []
+  const imageRenders = []
+
+  $('img').each((idx, e) => {
+    const file = path.resolve(basedir, e.attribs.src)
+    const image = transformImage(file, [1 / 7, 1 / 12], 80)
+    console.log(file, image)
+    imagePromises[idx] = image
+    imagePromises[idx].then(img => { imageRenders[idx] = img })
+  })
+
+  await Promise.all(imagePromises)
+
+  $('img').replaceWith(idx => imageRenders[idx])
 
   res.status(200)
-    .send(result)
+    .send($.html())
 })
 
 module.exports = router
